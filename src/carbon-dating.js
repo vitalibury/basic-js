fd871fd9f3885a873e9cfdbe9bb4917ff2fcae0c
Bury Vitali <vitalisbayer@gmail.com>
2020-10-31 02:46:39 +0300

    feat: implement dateSample function

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,7 +3,12 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
+module.exports = function dateSample(sampleActivity) {
+  // throw new CustomError('Not implemented');
   // remove line with error and write your code here
+  if (sampleActivity > 0 && sampleActivity <= MODERN_ACTIVITY && typeof sampleActivity === 'string') {
+    return Math.ceil(Math.log(MODERN_ACTIVITY / sampleActivity) / (0.693 / HALF_LIFE_PERIOD))
+  } else {
+    return false
+  }
 };
